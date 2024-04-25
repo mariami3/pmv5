dc1ada06cb7239c85967b81ed50a05d99f8e042e
mariami3 <mariami_petriashvili06@mail.ru>
2024-04-25 17:03:08 +0300

    u_u

---
+++ mmv/ContactDetailsViewModel/ContactDetails.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mmv
+
+{
+    public class ContactDetailsViewModel : ViewModelBase
+    {
+        private Contact selectedContact;
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged(nameof(SelectedContact));
+            }
+        }
+
+        public ICommand SaveContactCommand { get; }
+
+        public ContactDetailsViewModel()
+        {
+            SaveContactCommand = new RelayCommand(SaveContact);
+        }
+
+        private void SaveContact(object obj)
+        {
+        
+        }
+    }
+}
+
---
+++ mmv/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhoneBook
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Contact> contacts;
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return contacts; }
+            set
+            {
+                contacts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Contact selectedContact;
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ViewModelBase currentPage;
+        public ViewModelBase CurrentPage
+        {
+            get { return currentPage; }
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowContactListPageCommand { get; }
+        public ICommand ShowContactDetailsPageCommand { get; }
+
+        public MainViewModel()
+        {
+            ShowContactListPageCommand = new RelayCommand(ShowContactListPage);
+            ShowContactDetailsPageCommand = new RelayCommand(ShowContactDetailsPage);
+
+            ContactListViewModel contactListViewModel = new ContactListViewModel();
+            contactListViewModel.LoadContacts();
+            ContactDetailsViewModel contactDetailsViewModel = new ContactDetailsViewModel();
+
+            CurrentPage = contactListViewModel;
+        }
+
+        private void ShowContactListPage(object obj)
+        {
+            CurrentPage = new ContactListViewModel();
+        }
+
+        private void ShowContactDetailsPage(object obj)
+        {
+            CurrentPage = new ContactDetailsViewModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

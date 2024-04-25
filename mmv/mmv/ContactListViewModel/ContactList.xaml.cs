288be13a5fddf5f6aa29bbbb7328c9361778fd0a
mariami3 <144897983+mariami3@users.noreply.github.com>
2024-04-25 17:13:46 +0300

    Add files via upload

---
+++ mmv/mmv/ContactListViewModel/ContactList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace mmv
+
+{
+    public class ContactList : ViewModelBase
+    {
+        private ObservableCollection<Contact> contacts;
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return contacts; }
+            set
+            {
+                contacts = value;
+                OnPropertyChanged(nameof(Contacts));
+            }
+        }
+
+        public ICommand EditContactCommand { get; }
+
+        public ContactList()
+        {
+            EditContactCommand = new RelayCommand(EditContact);
+        }
+
+        public void LoadContacts()
+        {
+            Contacts = new ObservableCollection<Contact>
+            {
+                new Contact { Name = "John Doe", Phone = "123-456-7890", Email = "john.doe@example.com" },
+                new Contact { Name = "Jane Smith", Phone = "987-654-3210", Email = "jane.smith@example.com" }
+            };
+        }
+
+    }
+}
---
+++ mmv/mmv/Model/Contact.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace mmv
+
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged(nameof(Phone));
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                (nameof(Email));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
